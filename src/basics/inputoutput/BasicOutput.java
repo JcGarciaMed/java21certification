a1e2720b14e3d5a13c095cffbf7b59054f7b42eb
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2025-10-16 20:41:24 -0500

    feature/basic output

---
+++ src/basics/inputoutput/BasicOutput.java
@@ -0,0 +1,20 @@
+package basics.inputoutput;
+
+public class BasicOutput {
+    public static void main(String[] args) {
+        System.out.println("Hello World!" + " Java Programmer ");
+
+        System.out.println("Java ");
+        System.out.println("Programming ");
+        System.out.print("is ");
+        System.out.print("fun");
+
+        int a = 4;
+        int b = 6;
+        int c = a + b;
+        System.out.println();
+        System.out.printf("%d + %d = %d%n", a, b, c);
+        System.out.printf("%S", "hello");
+        System.out.print(" WORLD");
+    }
+}
