6c33805677dcf8f911f86ff5ef7f9bba84cad685
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2025-10-16 22:52:38 -0500

    feature/variables

---
+++ src/basics/variable/Variables.java
@@ -0,0 +1,24 @@
+package basics.variable;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10, c = 20, d = a+c;
+        System.out.println(a + c +d );
+
+        // Valid names
+        int age;
+        int _age;
+        int $age;
+        int ageOfEmpires;
+        int age$3;
+        int age_2;
+
+        /* invalid
+        int a ge;
+        int 3age;
+        int break;
+        int do;
+        int public;
+        */
+    }
+}
